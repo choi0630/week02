dc410788eecc75b19ac4983833865ef36ada2b4d
choi jungsoo <cjs030630@gmail.com>
2026-03-16 12:22:22 +0900

    과제 제출 1

--- app/src/main/java/com/example/week02/util/InputUtil.kt
+++ app/src/main/java/com/example/week02/util/InputUtil.kt
@@ -1,13 +1,19 @@
 package com.example.week02.util
 
 fun readInt(prompt: String): Int {
-    println(prompt)
+    while (true) { // 👈 숫자를 제대로 넣을 때까지 무한 반복
+        print(prompt) // println 대신 print를 쓰면 입력창이 같은 줄에 생겨 더 깔끔합니다.
+        val input = readlnOrNull()?.toIntOrNull()
 
-    return readlnOrNull()?.toIntOrNull() ?: -1
+        if (input != null) {
+            return input // 👈 정상적인 숫자면 즉시 반환하고 함수 종료
+        } else {
+            println("[오류] 숫자만 입력 가능합니다. 다시 시도해주세요.")
+        }
+    }
 }
 
 fun readString(prompt: String): String {
-    println(prompt)
-
+    print(prompt)
     return readlnOrNull() ?: ""
 }
